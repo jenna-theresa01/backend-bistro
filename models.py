0fe4aa6c14bc75c1e3cb676ab56f5124f1c4a819
Jenna Martin <jennamar828@gmail.com>
2023-11-09 21:25:26 +0000

    data started FINALLY in the tables

--- models.py
+++ models.py
@@ -1,3 +1,32 @@
-from typing import List
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
-from sqlalchemy.orm import 
+from .database import Base
+
+
+class Menu(Base):
+    __tablename__ = "menu"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    title: Mapped[str] = Column(String, default="Title")
+    description: Mapped[str] = Column(String, default="Description")
+    price: Mapped[int] = Column(Integer, default="Price")
+    spicy_level: Mapped[int] = Column(Integer, default="Spicy Level")
+    category_id: Mapped[int] = Column(ForeignKey("category.id"))
+    cuisine_id: Mapped[int] = Column(ForeignKey("cuisine.id"))
+
+    # is this where I need to put the foreign keys for cuisine and category?
+
+
+
+class Category(Base):
+    __tablename__ = "category"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(ForeignKey("category.id"))
+
+class Cuisine(Base):
+    __tablename__ = "cuisine"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(ForeignKey("cuisine.id"))
--- postgres.session.sql
+++ postgres.session.sql
@@ -1,2 +1,94 @@
-INSERT INTO category (id, name)
-VALUES (id: integer, 'name:character varying');
+INSERT INTO category (name)
+VALUES ('Appetizer');
+
+INSERT INTO category (name)
+VALUES ('Breakfast');
+
+INSERT INTO category (name)
+VALUES ('Lunch');
+
+INSERT INTO category (name)
+VALUES ('Dinner');
+
+INSERT INTO cuisine (name)
+VALUES ('Italian');
+
+INSERT INTO cuisine (name)
+VALUES ('American');
+
+INSERT INTO cuisine (name)
+VALUES ('Thai');
+
+INSERT INTO cuisine (name)
+VALUES ('Indian');
+
+INSERT INTO cuisine (name)
+VALUES ('Japanese');
+
+INSERT INTO menu (
+        title,
+        description,
+        price,
+        spicy_level,
+        category_id,
+        cuisine_id
+    )
+VALUES (
+        'Spaghetti Carbonara',
+        'A classic Roman pasta dish made with eggs, cheese, pancetta, and black pepper.',
+        13.99,
+        1,
+        4,
+        1
+    );
+
+    INSERT INTO menu (
+        title,
+        description,
+        price,
+        spicy_level,
+        category_id,
+        cuisine_id
+    )
+VALUES (
+        'Spaghetti Carbonara',
+        'A classic Roman pasta dish made with eggs, cheese, pancetta, and black pepper.',
+        13.99,
+        1,
+        4,
+        1
+    );
+
+    INSERT INTO menu (
+        title,
+        description,
+        price,
+        spicy_level,
+        category_id,
+        cuisine_id
+    )
+VALUES (
+        'Spaghetti Carbonara',
+        'A classic Roman pasta dish made with eggs, cheese, pancetta, and black pepper.',
+        13.99,
+        1,
+        4,
+        1
+    );
+
+    INSERT INTO menu (
+        title,
+        description,
+        price,
+        spicy_level,
+        category_id,
+        cuisine_id
+    )
+VALUES (
+        'Spaghetti Carbonara',
+        'A classic Roman pasta dish made with eggs, cheese, pancetta, and black pepper.',
+        13.99,
+        1,
+        4,
+        1
+    );
